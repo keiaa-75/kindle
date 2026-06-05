b5ec102c43560b356a9e8752ec140e43bdbf9c66
Keiaa <confinedrose@duck.com>
2026-06-05 21:01:10 +0800

    feat: write shared wrapper for routes

---
+++ src/routes/[org]/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { setContext } from 'svelte';
+	import { getFirebaseApp } from '$lib/firebase';
+
+	const { data, children } = $props();
+
+	setContext('org', () => data.org);
+	setContext('firebase', () => getFirebaseApp(data.org));
+</script>
+
+{@render children()}
